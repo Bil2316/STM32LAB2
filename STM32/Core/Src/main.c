2de293617f4bfbf12b772fd5c81e9d1ecd7e6363
Bil <ptbinh804@gmail.com>
2024-09-30 18:37:00 +0700

    Update function updateLEDMatrix

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -78,43 +78,43 @@ void updateLEDMatrix(int index)
 	{
 	case 0:
 		HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, 1);
-		HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, 0);
 		break;
 	case 1:
 		HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, 1);
-		HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, 0);
 		break;
 	case 2:
 		HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, 1);
-		HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, 0);
 		break;
 	case 3:
 		HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, 1);
-		HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, 0);
 		break;
 	case 4:
 		HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, 1);
-		HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, 0);
 		break;
 	case 5:
 		HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, 1);
-		HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, 0);
 		break;
 	case 6:
 		HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, 1);
-		HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, 0);
 		break;
 	case 7:
 		HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, 1);
-		HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, 0);
 		displayLEDMatrix(index);
+		HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, 0);
 		break;
 	default:
 		break;
